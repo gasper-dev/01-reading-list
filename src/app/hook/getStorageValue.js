56b3ba8d9fc5004a15d3ec678839be58434c8dd8
gasper-dev <gasper1one@gmail.com>
2023-07-18 21:10:47 -0600

    typeof window !== "undefined" && window.localStorage

--- src/app/hook/getStorageValue.js
+++ src/app/hook/getStorageValue.js
@@ -1,6 +1,7 @@
 // Función para obtener el valor inicial del estado desde localStorage
 export default function getStorageValue(key, defaultValue) {
-  const saved = localStorage.getItem(key);
+  const saved =
+    typeof window !== "undefined" && window.localStorage.getItem(key);
   const initial = JSON.parse(saved);
   return initial || defaultValue;
 }
